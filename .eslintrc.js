6d0985e1b03dea8378463dc456fedb0e59b81d6d
nimowagukari <nimowagukari@outlook.com>
2023-04-02 14:43:12 +0000

    開発設定を諸々修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,22 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es2021: true,
-    node: true,
   },
   extends: ["eslint:recommended", "prettier"],
-  overrides: [],
+  overrides: [
+    {
+      files: [".eslintrc.js", "jest.config.js", "webpack.config.js"],
+      env: {
+        commonjs: true,
+        node: true,
+      },
+    },
+  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
   rules: {},
+  ignorePatterns: ["/dist/**/*.js"],
 };
