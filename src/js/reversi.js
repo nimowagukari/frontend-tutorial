b103af664215a61f36bfc9fb4810dfaabe4ced7d
nimowagukari <nimowagukari@outlook.com>
2023-05-05 14:44:48 +0000

    リバーシを改修

--- src/js/reversi.js
+++ src/js/reversi.js
@@ -3,10 +3,17 @@ import { Field, GameMaker, Player } from "./modules/reversi";
 // フィールドを描画
 const fieldElement = document.querySelector(".field");
 const field = new Field(fieldElement);
-field.initializeCell();
 
-const player1 = new Player("white");
-const player2 = new Player("black");
+// プレイヤーを生成
+const player1 = new Player("black", field);
+const player2 = new Player("white", field);
 
-const game = new GameMaker(field, player1, player2);
-game.startGame();
+// フィールドにプレイヤーを設定
+field.setPlayers(player1, player2);
+
+const game = new GameMaker(field);
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", game.startGame);
+startButton.disabled = false;
+const skipButton = document.querySelector("#skip-button");
+skipButton.disabled = true;
